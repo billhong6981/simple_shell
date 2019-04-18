0f540e59da33471da2254e46771d2639a1c2a257
billhong6981 <bill.burton6981@gmail.com>
2019-04-18 02:19:20 +0000

    remove _getline

--- simple_shell.c
+++ simple_shell.c
@@ -8,19 +8,21 @@ int main(void)
 {
 	list_t *head = NULL;
 	size_t len = 0;
-	char *cmd, *av1 = NULL, *new_line, *line = NULL;
+	char *cmd, *dir, *av1 = NULL, *new_line, *line = NULL;
 	char *(*found_builtin)(char *, list_t *);
 
 	path_list(&head);
 	while (1)
 	{
+		dir = _getcwd();
+		write(1, dir, _strlen(dir));
 		write(1, "$ ", 2);
-		if ((_get_line(&line, &len, stdin)) == -1)
+		free(dir);
+		if ((getline(&line, &len, stdin)) == -1)
 		{
 			break;
 		}
 		new_line = trun_space(line);
-		free(line);
 		if (new_line == NULL)
 			continue;
 		found_builtin = get_builtin_fn(new_line, &av1);
@@ -39,6 +41,7 @@ int main(void)
 		else
 			exec_cmd(cmd, new_line);
 	}
+	free(line);
 	free_list(head);
 	return (0);
 }
